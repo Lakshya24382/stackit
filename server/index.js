1f3678d7437ee76196b72bfdf861c290ac65e64a
Lakshya Modi <lakshyamodi@Lakshyas-MacBook-Air.local>
2026-06-07 20:56:12 +0530

    fix wildcard route for express v5

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use('/api/notifications', require('./routes/notifications'));
 // Serve React build in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
